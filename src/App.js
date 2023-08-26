15dbbe017f891a53735c87db9f49a18d8d737742
pranshulgupta03 <guptapranshu0801@gmail.com>
2023-08-26 21:19:25 +0530

    app.js file modified

--- src/App.js
+++ src/App.js
@@ -3,19 +3,38 @@ import './App.css';
 import NavBar from './NavBar';
 import HomeCarsouel from './HomeCarsouel';
 import ImageCard from './ImageCard';
-import imagedata from './imagedata';
+import { useEffect, useState } from 'react';
+// import imagedata from './imagedata';
+
 const ncard = (val) => {
-  return <ImageCard key={val.id} title={val.title} imgsrc={val.imgsrc} desc={val.desc} />
+  return <ImageCard key={val.id} title={val.title} imgsrc={val.url} desc={val.desc} />
 }
 
 
 function App() {
+
+  const [Data, setData] = useState([]);
+
+  useEffect(() =>{
+    const fetchdata = async () =>{
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/photos/')
+        const data = await response.json();
+        setData(data)
+      }
+      catch(error) {
+        console.log("Error occured", error);
+      }
+    }
+    fetchdata();
+  }, []);
+
   return (
     <div>
       <NavBar />
       <HomeCarsouel />
       <div className="row">
-        {imagedata.map((val, index) => (   // here imagedata is a variable name
+        {Data.map((val, index) => (   // here imagedata is a variable name
           <div key={val.id} className="col-md-3">{ncard(val)}</div>
             ))}
       </div>
